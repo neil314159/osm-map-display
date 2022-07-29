cad89b5e1b6e63d50cf81a16a7841fb3dc0720d3
neil314159 <neil@neilboland.com>
2022-07-29 13:38:27 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,16 @@
+from prettymapp.geo import get_aoi
+from prettymapp.osm import get_osm_geometries
+from prettymapp.plotting import Plot
+from prettymapp.settings import STYLES
+
+aoi = get_aoi(address="Galway, Ireland", distance=500, rectangular=True)
+df = get_osm_geometries(aoi=aoi)
+
+fig = Plot(
+    df=df,
+    aoi_bounds=aoi.bounds,
+    draw_settings=STYLES["Peach"]
+).plot_all()
+
+fig.set_size_inches(8, 8)
+fig.savefig('test2png.png', dpi=100)
